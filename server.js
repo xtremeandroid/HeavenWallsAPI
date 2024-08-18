f49955d9c39ab012a55593015365ff2b65bfedc2
Ayush Singh <xtremeandpvt@gmail.com>
2024-08-18 14:08:37 +0530

    disabled cors

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //fix cors issue
-app.use(
-  cors({
-    origin: "https://heavenwalls.netlify.app",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 //cookie parse middleware
 app.use(cookieParser());
